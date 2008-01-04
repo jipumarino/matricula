e701151b0ea338aa75d4cd12a4e2853b7fe7be92
jipumarino <jipumarino@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2008-01-04 12:35:00 +0000

    Reporte de resumen de matriculados por nivel
    git-svn-id: http://svn2.assembla.com/svn/matricula2/trunk@52 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,4 +12,13 @@ class ReportsController < ApplicationController
 
     render :layout => 'print'
   end
+
+  def summary
+    @students_amounts = {}
+    levels = %w{PK K 1B 2B 3B 4B 5B 6B 7B 8B 1M 2M 3M 4M}
+    levels.each do |l|
+      @students_amounts[l] = Student.find_all_by_next_level(l, :order => 'next_level, fathers_name, mothers_name').size
+    end
+  end
+
 end
