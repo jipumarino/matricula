7501a23f48645fca6835d59df8027d604a4085c4
jipumarino <jipumarino@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2008-03-18 18:41:58 +0000

    Generar libro de pagos para alumnos no asignados a un curso
    git-svn-id: http://svn2.assembla.com/svn/matricula2/trunk@62 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -48,11 +48,11 @@ class ReportsController < ApplicationController
   
 def payments_book
     @students = {}
-    levels = %w{PK K 1B 2B 3B 4B 5B 6B 7B 8B 1M 2M 3M 4M}
-    sections = %w{A B C D}
-    levels.each do |l|
+    @levels = %w{PK K 1B 2B 3B 4B 5B 6B 7B 8B 1M 2M 3M 4M}
+    @sections = [""]+%w{A B C D}
+    @levels.each do |l|
       @students[l]= {}
-      sections.each do |s|
+      @sections.each do |s|
         @students[l][s] = Student.find_all_by_next_level_and_section(l, s, :order => 'next_level, section, fathers_name, mothers_name')
       end
     end
