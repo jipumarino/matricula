9ccd4e63b2d79eaadcebf8fc4f2ef96814dd5355
jipumarino <jipumarino@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2008-03-18 14:32:20 +0000

    Agregado reporte de matriculados por curso
    git-svn-id: http://svn2.assembla.com/svn/matricula2/trunk@57 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,7 @@ class ReportsController < ApplicationController
     @students = Student.find_all_by_next_level(params[:student][:level], :order => 'fathers_name, mothers_name')
   end
 
-  def total
+  def total_by_level
     @students = {}
     levels = %w{PK K 1B 2B 3B 4B 5B 6B 7B 8B 1M 2M 3M 4M}
     levels.each do |l|
@@ -13,6 +13,21 @@ class ReportsController < ApplicationController
     render :layout => 'print'
   end
 
+  def total_by_section
+    @students = {}
+    levels = %w{PK K 1B 2B 3B 4B 5B 6B 7B 8B 1M 2M 3M 4M}
+    sections = %w{A B C D}
+    levels.each do |l|
+      @students[l]= {}
+      sections.each do |s|
+        @students[l][s] = Student.find_all_by_next_level_and_section(l, s, :order => 'next_level, section, fathers_name, mothers_name')
+      end
+    end
+
+    render :layout => 'print'
+  end
+
+  
   def summary
     @students_amounts = {}
     levels = %w{PK K 1B 2B 3B 4B 5B 6B 7B 8B 1M 2M 3M 4M}
